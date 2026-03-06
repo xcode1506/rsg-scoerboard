01349940a9b15ce67f305da8711dc4034b34c3da
xcode1506 <xcode.admirer@gmail.com>
2026-03-06 20:58:40 +0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = {}
+
+Config.ToggleCommand = 'scoreboard'
+
+Config.Jobs = {
+    Medic = {'medic'},
+    Lawman = {'vallaw', 'blklaw'},
+    Wagon = {'wagon_blackwater'},
+    Saloon = {'valsaloontender', 'blasaloontender'},
+    Ranch = {'macfarranch'}
+}
+
+Config.RefreshInterval = 1500
+Config.Debug = false  -- Matikan debug
+
+Config.UI = {
+    position = "bottom-right",
+    scale = 0.8,
+    theme = "western",
+    timeFormat = 12
+}
